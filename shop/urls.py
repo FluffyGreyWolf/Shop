9e64549ff3c1ec6c99d41019ee0b0e2f7409fbc3
GreyWolf <szymonstrozynskiii@gmail.com>
2021-07-15 15:33:49 +0200

    update: filtering items by price

--- shop/urls.py
+++ shop/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.home),
     path('search-result', views.productSearch, name='search-result'),
+    path('search-result-price-filter', views.productPriceFilter, name='search-result-price-filter'),
     path('product-detail/<int:pk>', views.productDetail, name='product-detail'),
 ]
--- shop/views.py
+++ shop/views.py
@@ -1,3 +1,4 @@
+from django.db.models.query_utils import RegisterLookupMixin
 from django.shortcuts import redirect, render, get_object_or_404
 from .models import Product
 from itertools import chain
@@ -16,9 +17,28 @@ def productSearch(request):
         products1 = Product.objects.filter(name__contains=searched)
         products2 = Product.objects.filter(category__contains=searched)
         products = list(chain(products1, products2))
-        context = {'searched': searched, 'products': products}
+        context = {'products': products, 'searched': searched}
         return render(request, 'search-result.html', context)
     
+# View for filtering products by price
+def productPriceFilter(request):
+    if request.method == "GET":
+        min = request.GET['price-min']
+        max = request.GET['price-max']
+        searched = request.GET['products']
+    if not min:
+        min = 0
+    if not max:
+        max = 50000
+    products1 = Product.objects.filter(name__contains=searched).filter(price__gte=min).filter(price__lte=max)
+    products2 = Product.objects.filter(category__contains=searched).filter(price__gte=min).filter(price__lte=max)
+    products = list(chain(products1, products2))
+    context = {'products': products, 'searched': searched}
+    return render(request, 'search-result.html', context)
+
+
+
+
 # View for detailed page of product
 def productDetail(request, pk):
     product = get_object_or_404(Product, pk=pk)
