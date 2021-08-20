7e0e74fb7607e0dcfa68b48c9c0d2c3d8777e072
GreyWolf <szymonstrozynskiii@gmail.com>
2021-08-20 15:42:08 +0200

    fix: added responsiveness to navbar

--- static/script.js
+++ static/script.js
@@ -1,9 +1,24 @@
 // Change visibility of category dropdown in navbar
-document.getElementById("nav-dropdown").addEventListener("click", function(){$('.nav-subitem').toggle();});
+document.getElementById("navbar-dropdown").addEventListener("click", function(){$('.dropdown-item').toggle();});
 
 // Closes category dropdown on click anywhere on page
 $('body').click(function(e) {
-    if ($(e.target).closest('.nav-dropdown').length === 0) {
-        $('.nav-subitem').hide();
+    if ($(e.target).closest('.cant-close').length === 0) {
+        $('.dropdown-item').hide();
     }
 });
+
+// Change visibility of mobile menu items
+document.getElementById("toogle-menu").addEventListener("click", function(){$('.navbar-links').toggle();});
+
+// Change visibility of components menu
+document.getElementById("components").addEventListener("click", function(){$('.dropdown-components').toggle();});
+
+// Change visibility of accessories menu
+document.getElementById("accessories").addEventListener("click", function(){$('.dropdown-accessories').toggle();});
+
+// Change visibility of  peripherials menu
+document.getElementById("peripherials").addEventListener("click", function(){$('.dropdown-peripherials').toggle();});
+
+// Change visibility of mobile-devices menu
+document.getElementById("mobile-devices").addEventListener("click", function(){$('.dropdown-mobile-devices').toggle();});
